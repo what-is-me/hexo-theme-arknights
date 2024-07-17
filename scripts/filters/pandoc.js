414c20cddb6d03514d25718abcb563446b1ed3d2
Leo.Cai <2454787428@qq.com>
2024-07-17 18:12:51 +0800

    h font-size

--- scripts/filters/pandoc.js
+++ scripts/filters/pandoc.js
@@ -4,7 +4,7 @@
 
 function initHeader(str) {
   return str.replaceAll(/<h[1-6] id=".*?">.*?<\/h[1-6]>/g, (match) => {
-    let link = match.match(/(?<=id=").*?(?=")/)[0],
+    const link = match.match(/(?<=id=").*?(?=")/)[0],
       pre = match.match(/<h[1-6] id=".*?">/)[0],
       nxt = match.slice(pre.length)
     if (!match.match(/<a href="#.*?" class="headerlink" title=".*?"><\/a>/g)) {
@@ -14,8 +14,20 @@ function initHeader(str) {
   })
 }
 
-hexo.extend.filter.register('after_post_render', (data) => {
-  data.content = initHeader(data.content)
-  data.more = initHeader(data.more)
+function initHeader2(str) {
+  return str.replaceAll(/<h[1-6] id=".*?"><a href=".*?" class="headerlink" title=".*?"><\/a>.*?<\/h[1-6]>/g, (match) => {
+    return match.replace(/(<h[1-6] id=".*?"><a href=".*?" class="headerlink" title=".*?"><\/a>)(.*?)(<\/h[1-6]>)/, '$1<span class="header-auto-count">$2</span>$3')
+  })
+}
+
+function apply(data, ...fns) {
+  for (const fn of fns) {
+    data.content = fn(data.content)
+    data.more = fn(data.more)
+  }
   return data
+}
+
+hexo.extend.filter.register('after_post_render', (data) => {
+  return apply(data, initHeader, initHeader2)
 });
